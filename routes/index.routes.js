14412ff77e801ec1c9bbe1cdda4a4c748b3d76ab
Mariana Ribeiro de Carvalho <mariana.rcarvalho.992gmail.com>
2023-07-16 16:57:14 +0100

    plant database parameters edited. Sunlight

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const Plant = require ("../models/Plant.model")
+const Plant = require("../models/Plant.model")
 const idealClimate = require("../utils/idealClimate_Values")
-const directSunlightH = require("../utils/directSunlight_Values");
+const sunlight = require("../utils/sunlight_Values");
 const soilType = require("../utils/soilType_Values");
 const organicMatter = require("../utils/organicMatter_Values");
 const plantType = require("../utils/plantType_Values");
@@ -15,16 +15,32 @@ router.get("/", (req, res, next) => {
 //GET add plant to database page
 router.get("/add_plant", (req, res, next) => {
   //console.log(req);
-  res.render("add_plant", {idealClimate, directSunlightH, soilType, organicMatter, plantType});
+  res.render("add_plant", { idealClimate, sunlight, soilType, organicMatter, plantType });
+});
+
+router.post("/add_plant", async (req, res) => {
+  const { plantName, species, idealClimate, sunlight, soilType, organicMatter, plantType, extraInfo, image } = req.body;
+
+  try {
+    const newPlant = await Plant.create({ plantName, species, idealClimate, sunlight, soilType, organicMatter, plantType, extraInfo, image });
+
+    res.redirect("/new_plant_added")
+  } catch (error) {
+    console.log(error)
+  }
+});
+
+router.get("/new_plant_added", (req, res) => {
+  res.render("new_plant_added");
 });
 
 router.get("/suggestion_request", (req, res) => {
   res.render("suggestion_request");
-})
+});
 
 router.post("/suggestion_request", (req, res) => {
   const envData = req.body;
-})
+});
 
 /* GET test page */
 router.get("/test", (req, res, next) => {
