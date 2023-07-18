746ea014b95ca0703a01d2348d3259faf819f884
Peter R <prodger2@gmail.com>
2023-07-18 19:18:06 +0200

    Created the Suggestion object

--- models/Suggestion.model.js
+++ models/Suggestion.model.js
@@ -1,34 +1,30 @@
 const { Schema, model } = require("mongoose");
-
-const suggestionSchema = new Schema({
-  suggestedToUserId:{
-    type: Number, //--------------RELATION
-    required: true,
+const suggestionSchema = new Schema(
+  {
+    suggestedToUserId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    environmentInput: {
+      type: Object,
+      required: true,
+    },
+    plantSuggestionId: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Plant",
+      },
+    ],
+    thumbsUp: {
+      type: Number,
+      default: 0,
+    },
+    thumbsDown: {
+      type: Number,
+      default: 0,
+    },
   },
-  environmentInput: {
-    type: Object,
-    required: true
-  },
-  plantSuggestionId: {
-    type: Number //--------------RELATION
-  },
-  timeStamp: {
-    type: Date,
-    default: Date.now
-  },
-  plantSuggestionId: {
-    type: Number
-  },
-  thumbsUp: {
-    type: Number,
-    default: 0,
-  },
-  thumbsDown: {
-    type: Number,
-    default: 0,
-  },
-});
-
+  { timestamps: true }
+);
 const Suggestion = model("Suggestion", suggestionSchema);
-
 module.exports = Suggestion;
